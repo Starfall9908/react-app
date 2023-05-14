d7a00068e3b6a18c23284527eb64f23577affa26
Starfall9908 <starfall9908@gmail.com>
2023-05-14 15:40:48 +0200

    Added Button and Alert and made List Group more dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
-function App(){
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["Vega", "Sirius", "Rigel", "Betelgeus", "Antares"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [alertVisible, setAlertVisisbility] = useState(false);
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Stars"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisisbility(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisisbility(true)}>Big Button</Button>
+    </div>
+  );
 }
 
 export default App;
